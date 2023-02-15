86a5710a7ed5f1a4a132cd129fe27ff47a64c6ce
Oana Surdea <oana.surdea@ness.com>
2023-02-16 00:24:07 +0200

    feat(ui): add taiga-ui (charts, table, editor & preview)

--- apps/ng-kanbanly/src/app/app.module.ts
+++ apps/ng-kanbanly/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TuiAlertModule, TuiButtonModule, TuiDialogModule, TuiRootModule, TUI_SANITIZER } from '@taiga-ui/core';
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, BrowserAnimationsModule, TuiRootModule, TuiDialogModule, TuiAlertModule, TuiButtonModule],
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
