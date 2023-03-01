37c5c7c9ae8becca2bc4b1c80a1237a18be47506
Oana Surdea <oana.surdea@ness.com>
2023-03-01 23:27:02 +0200

    tests(ng-kanbanly) update test dependencies

--- apps/ng-kanbanly/src/app/board/board.component.spec.ts
+++ apps/ng-kanbanly/src/app/board/board.component.spec.ts
@@ -1,6 +1,9 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { TuiScrollbarModule } from '@taiga-ui/core';
+import { environment } from '../../environments/environment';
 
 import { BoardComponent } from './board.component';
 
@@ -10,7 +13,11 @@ describe('BoardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TuiScrollbarModule],
+      imports: [
+        TuiScrollbarModule,
+        provideFirestore(() => getFirestore()),
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+      ],
       declarations: [BoardComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
